bfd8affeb11a19f3c598db5569abae4d6a73864e
TyRoopam9599 <roopamtyagi72@gmail.com>
2023-11-02 22:00:02 +0530

    CORS Changes

--- backend/App.js
+++ backend/App.js
@@ -8,8 +8,7 @@ require('./database/Connect');
 const app=express();
 app.use(express.json({ limit: '8mb' }));
 app.use(express.urlencoded({ limit: '8mb', extended: true }));
-app.use(cors({origin: "*"}));
-
+app.use(cors({origin: "*", credentials:"*"}));
 app.use('/auth', authRoutes);
 
 PORT=process.env.PORT
