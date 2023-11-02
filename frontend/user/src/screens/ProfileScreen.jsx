b37e79b3c3760590ed11afe8147ed459ffc9c915
Ratan Deep Singh <91245947+ratan2020cs131@users.noreply.github.com>
2023-11-03 01:37:41 +0530

    Profile UI Changed

--- frontend/user/src/screens/ProfileScreen.jsx
+++ frontend/user/src/screens/ProfileScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Pressable } from 'react-native';
+import { View, Text, StyleSheet, Pressable, TouchableOpacity } from 'react-native';
 import { FontAwesome, MaterialIcons, Entypo, Feather, Ionicons } from '@expo/vector-icons';
 import ScreenWrapper from './ScreenWrapper';
 import GlobalStyles from '../GlobalStyles';
@@ -9,52 +9,56 @@ const ProfileScreen = ({ navigation }) => {
     return (
         <ScreenWrapper title={"Profile"}>
             <View style={styles.container}>
-                {/*UserName */}
-                {/* Name */}
-                {/* Name */}
-                <View style={styles.section}>
-                    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', flex: 1, paddingRight: 10 }}>
-                        <FontAwesome name='user-o' size={15} color={"black"} />
-                        <Text style={[GlobalStyles.normalText, styles.input2]}>Name</Text>
+
+                <View style={{width:'100%', alignItems:'center'}}>
+
+                    {/* Name */}
+                    <View style={styles.section}>
+                        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', flex: 1, paddingRight: 10 }}>
+                            <FontAwesome name='user-o' size={15} color={"black"} />
+                            <Text style={[GlobalStyles.normalText, styles.input2]}>Name</Text>
+                        </View>
+                        <Text>{UserData[0].fname + " " + UserData[0].lname}</Text>
                     </View>
-                    <Text>{UserData[0].fname + " " + UserData[0].lname}</Text>
+
+                    {/* Email */}
+                    <View style={styles.section}>
+                        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', flex: 1, paddingRight: 10 }}>
+                            <MaterialIcons name='alternate-email' size={15} color={"black"} />
+                            <Text style={[GlobalStyles.normalText, styles.input2]}>Email</Text>
+                        </View>
+                        <Text>{UserData[0].email}</Text>
+                    </View>
+
+                    {/* Phone Number */}
+                    <View style={styles.section}>
+                        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', flex: 1, paddingRight: 10 }}>
+                            <Feather name='phone' size={15} color={"black"} />
+                            <Text style={[GlobalStyles.normalText, styles.input2]}>Phone Number</Text>
+                        </View>
+                        <Text>{UserData[0].phone}</Text>
+                    </View>
+
+                    {/* Password */}
+                    <View style={[styles.section, { marginBottom: 25 }]}>
+                        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', flex: 1, paddingRight: 10 }}>
+                            <Feather name='lock' size={15} color={"black"} />
+                            <Text style={[GlobalStyles.normalText, styles.input2]}>Password</Text>
+                        </View>
+                        <Text>{UserData[0].password}</Text>
+                    </View>
+
+                    <TouchableOpacity style={[GlobalStyles.button, { flexDirection: 'row', alignItems: 'center', gap: 15, backgroundColor:'#1E1F22' }]} onPress={() => navigation.navigate("ProfileUpdate", { userData: UserData[0] })}>
+                        <Text style={[GlobalStyles.semiBoldText, { color:'#E9E5D7' }]}>UPDATE PROFILE</Text>
+                        {/* <FontAwesome name='pencil' size={20} color={"#E9E5D7"} /> */}
+                    </TouchableOpacity>
                 </View>
 
-                {/* Email */}
-                <View style={styles.section}>
-                    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', flex: 1, paddingRight: 10 }}>
-                    <MaterialIcons name='alternate-email' size={15} color={"black"} />
-                        <Text style={[GlobalStyles.normalText, styles.input2]}>Email</Text>
-                    </View>
-                    <Text>{UserData[0].email}</Text>
-                </View>
-
-                {/* Phone Number */}
-                <View style={styles.section}>
-                    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', flex: 1, paddingRight: 10 }}>
-                        <Feather name='phone' size={15} color={"black"} />
-                        <Text style={[GlobalStyles.normalText, styles.input2]}>Phone Number</Text>
-                    </View>
-                    <Text>{UserData[0].phone}</Text>
-                </View>
-
-                {/* Password */}
-                <View style={[styles.section, {marginBottom: 25}]}>
-                    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', flex: 1, paddingRight: 10 }}>
-                        <Feather name='lock' size={15} color={"black"} />
-                        <Text style={[GlobalStyles.normalText, styles.input2]}>Password</Text>
-                    </View>
-                    <Text>{UserData[0].password}</Text>
-                </View>
-
-                <Pressable style={[GlobalStyles.button, { flexDirection: 'row', alignItems: 'center', marginBottom: 10 }]} onPress={() => navigation.navigate("ProfileUpdate", {userData:UserData[0]})}>
-                    <FontAwesome name='pencil' size={20} color={"black"} />
-                    <Text style={[GlobalStyles.normalText, { fontSize: 20 }]}>{" "}Update</Text>
-                </Pressable>
-                <Pressable style={[GlobalStyles.button, { flexDirection: 'row', alignItems: 'center' }]}>
-                    <MaterialIcons name='logout' size={20} color={"black"} />
-                    <Text style={[GlobalStyles.normalText, { fontSize: 20 }]}>{" "}Logout</Text>
-                </Pressable>
+                {/* LOGOUT BUTTON */}
+                <TouchableOpacity style={[GlobalStyles.button, { flexDirection: 'row', alignItems: 'center', gap: 5 }]}>
+                    <Text style={[GlobalStyles.boldText, { paddingLeft:10}]}>LOGOUT</Text>
+                    <Ionicons name="log-out-outline" style={{ fontSize: 23 }}></Ionicons>
+                </TouchableOpacity>
             </View>
         </ScreenWrapper>
     )
@@ -65,7 +69,9 @@ export default ProfileScreen;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: 'space-between',
+        paddingVertical:30,
+        paddingTop:60,
         alignItems: 'center',
     },
     section: {
@@ -83,10 +89,9 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingHorizontal: 8,
         fontSize: 15,
-        
+
     }
 })
-
 
 const UserData = [
     {
