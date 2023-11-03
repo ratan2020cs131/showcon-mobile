0da11939e466d515a25cfe70fa93572bf103529c
Ratan Deep Singh <91245947+ratan2020cs131@users.noreply.github.com>
2023-11-03 14:55:35 +0530

    movie api done

--- backend/App.js
+++ backend/App.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config({ path: './.env' });
 const authRoutes = require('./routes/Auth');
+const movieRoute = require('./routes/Movie');
 require('./database/Connect');
 
 const app=express();
@@ -12,6 +13,7 @@ app.use(express.urlencoded({ limit: '8mb', extended: true }));
 app.use(cors({origin: "*", credentials:"*"}));
 
 app.use('/auth', authRoutes);
+app.use('/movie', movieRoute);
 
 PORT=process.env.PORT
 app.listen(PORT, () => {
---
+++ backend/routes/Cinema.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const route = express.Router();
+const { AuthMiddleWare } = require('../middleware/AuthMiddleware.js')
+const { } = require('../controller/CinemaController.js');
+
+
+route.get('/:id', );
+
+module.exports = route;
