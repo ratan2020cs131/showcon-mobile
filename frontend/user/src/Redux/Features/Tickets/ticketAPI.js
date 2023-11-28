d4793ee977dd09d4ab3e1cb16255042a675440aa
Ratan Deep Singh <91245947+ratan2020cs131@users.noreply.github.com>
2023-11-28 11:20:14 +0530

    smart contract init

--- frontend/user/src/Redux/Features/Tickets/ticketAPI.js
+++ frontend/user/src/Redux/Features/Tickets/ticketAPI.js
@@ -3,7 +3,6 @@ import BASE_URL from "../../../api/BaseUrl";
 
 const createTicket = async (data) => {
     try {
-        console.log("hi from api: ",data);
         const response = await axiosToken.post(`${BASE_URL}ticket/`, data);
         return response.data;
     } catch (err) {
