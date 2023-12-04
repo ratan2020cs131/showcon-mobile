a9c2c3e6325ff1e049565f878db9c819d582e181
TyRoopam9599 <roopamtyagi72@gmail.com>
2023-12-04 15:14:13 +0530

    Screens Topbar Created

---
+++ frontend/cinema/src/components/Topbar.jsx
@@ -0,0 +1,29 @@
+import { View, SafeAreaView, StyleSheet, Text } from "react-native";
+import GlobalStyles from "../GlobalStyles";
+
+const TopBar = ({ title }) => {
+    return (
+        <SafeAreaView>
+            <View
+                style={styles.container}
+            >
+                {title && <Text style={[GlobalStyles.boldText, styles.title]}>{title}</Text>}
+            </View>
+        </SafeAreaView>
+    );
+};
+
+export default TopBar;
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#1E1F22',
+        width: '100%',
+        paddingHorizontal: 20,
+        paddingVertical: 15
+    },
+    title: {
+        color: '#F55139',
+        fontSize: 20
+    }
+})
