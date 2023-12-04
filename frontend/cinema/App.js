116b89b1502ed95e046f36f47e88d2e13f5ef10e
TyRoopam9599 <roopamtyagi72@gmail.com>
2023-12-04 15:13:18 +0530

    Add Screen

--- frontend/cinema/App.js
+++ frontend/cinema/App.js
@@ -1,18 +1,20 @@
-import { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
-import { useFonts } from 'expo-font';
+import { useEffect, useState } from "react";
+import { View, Text } from "react-native";
+import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
+import MovieScreen from "./src/screens/MovieScreen";
+import Scanner from "./src/screens/Scanner";
 
-export default function App({ }) {
+export default function App({}) {
   const [isLogged, setIsLogged] = useState(undefined);
 
   const [fontsLoaded] = useFonts({
-    'Montserrat-Bold': require('./assets/fonts/Montserrat-Bold.ttf'),
-    'Montserrat-Regular': require('./assets/fonts/Montserrat-Regular.ttf'),
-    'Montserrat-SemiBold': require('./assets/fonts/Montserrat-SemiBold.ttf'),
-    'OpenSans-Bold': require('./assets/fonts/OpenSans-Bold.ttf'),
-    'OpenSans-Regular': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'OpenSans-SemiBold': require('./assets/fonts/OpenSans-SemiBold.ttf'),
+    "Montserrat-Bold": require("./assets/fonts/Montserrat-Bold.ttf"),
+    "Montserrat-Regular": require("./assets/fonts/Montserrat-Regular.ttf"),
+    "Montserrat-SemiBold": require("./assets/fonts/Montserrat-SemiBold.ttf"),
+    "OpenSans-Bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+    "OpenSans-Regular": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "OpenSans-SemiBold": require("./assets/fonts/OpenSans-SemiBold.ttf"),
   });
 
   useEffect(() => {
@@ -25,7 +27,6 @@ export default function App({ }) {
     }, 2000);
   }, []);
 
-
   if (!fontsLoaded) {
     return null;
   } else {
@@ -34,10 +35,10 @@ export default function App({ }) {
     }, 1000);
   }
 
-
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>showcon-cinema</Text>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      {/* <MovieScreen /> */}
+      <Scanner />
     </View>
   );
 }
