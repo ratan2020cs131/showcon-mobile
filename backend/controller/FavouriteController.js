5efaeb10e953b8f8b73a26de699cabbab77633a6
Ratan Deep Singh <91245947+ratan2020cs131@users.noreply.github.com>
2024-01-05 14:20:02 +0530

    favourite, history api updated

--- backend/controller/FavouriteController.js
+++ backend/controller/FavouriteController.js
@@ -2,24 +2,24 @@ const Favourite = require("../database/models/Favourite");
 const Movie = require("../database/models/Movie")
 
 const FavouriteController = async (req, res) => {
-    try{
-        const userId = req.user._id ;
-        if(!userId) return res.status(404).json({message: "User Not Found"})
+    try {
+        const userId = req.user._id;
+        if (!userId) return res.status(400).json({ message: "User Not Found" })
 
-        const movieID = req.body.MovieID ;
-        const movie = await Movie.findOne({_id: movieID})
-        if(!movie) return res.status(404).json({message: "Movie not Found"});
+        const movieID = req.body.MovieID;
+        const movie = await Movie.findOne({ _id: movieID })
+        if (!movie) return res.status(404).json({ message: "Movie not Found" });
 
         let favourite = await Favourite.findOne({ userId });
 
         if (!favourite) {
             favourite = new Favourite({ userId, list: [] });
         }
-        
+
         if (!favourite.list.includes(movieID)) {
             favourite.list.push(movieID);
             await favourite.save();
-            return res.status(200).json({ message: "Added to favorites" });
+            return res.status(201).json({ message: "Added to favorites" });
         } else {
             return res.status(200).json({ message: "Movie already in favorites" });
         }
@@ -28,16 +28,18 @@ const FavouriteController = async (req, res) => {
     }
 };
 
-const getFavouriteController = async(req, res) => {
+const getFavouriteController = async (req, res) => {
     try {
-        const userId = req.user._id ;
-        if(!userId) return res.status(404).json({message: "User Not Found"})
-
-        const fav = await Favourite.findOne({userId})
-
-        res.status(200).send(fav) ;
+        const userId = req.user._id;
+        if (!userId) return res.status(404).json({ message: "User Not Found" })
+        const fav = await Favourite.findOne({ userId })
+        if (fav) {
+            res.status(200).send({ "movieId": fav.list });
+        } else {
+            res.status(200).send([]);
+        }
     }
-    catch(err) {
+    catch (err) {
         console.log(err)
     }
 }
