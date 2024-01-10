2d71e385e300d86dbfa4ba0328c8a06dad6b01d7
Ratan Deep Singh <91245947+ratan2020cs131@users.noreply.github.com>
2024-01-11 01:51:44 +0530

    smart contract deployed

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.1",
+  networks:{
+    sepolia:{
+      url:process.env.SEPOLIA_URL,
+      accounts:[process.env.KEY]
+    }
+  }
 };
