60278194506fff85649301dad7ef3fab9d037484
Ratan Deep Singh <91245947+ratan2020cs131@users.noreply.github.com>
2024-02-02 11:49:27 +0530

    mime-type for image added in admin

--- frontend/admin/src/utils/ImageApi.js
+++ frontend/admin/src/utils/ImageApi.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import BASE_URL from './baseUrl';
 
-export const uploadImage = async (image) => {
+export const uploadImage = async (image, type) => {
     try {
         const formData = new FormData();
         formData.append('image', {
             uri: image,
-            type: 'image/png', // or the MIME type of your image
+            type: type, // or the MIME type of your image
             name: 'poster', // the name the server will receive the file as
         });
         const config = {
