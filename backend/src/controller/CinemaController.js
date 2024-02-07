fa8456ce84e3339507afe004f1306b3b387a0d2b
Ratan Deep Singh <91245947+ratan2020cs131@users.noreply.github.com>
2024-02-07 11:20:26 +0530

    cinema seat mapping done

--- backend/src/controller/CinemaController.js
+++ backend/src/controller/CinemaController.js
@@ -1,8 +1,35 @@
 const Cinema = require("../database/models/Cinema");
 
+const arrangeSeats = (seatsArray) => {
+    // Find the maximum seat number across all rows
+    let maxSeatNumber = 0;
+    seatsArray.forEach(item => {
+        if (maxSeatNumber < Math.max(...item.seats)) {
+            maxSeatNumber = Math.max(...item.seats)
+        }
+    });
+    // Initialize a new array to store arranged seats
+    const arrangedSeats = [];
+    // Iterate over each row of seats
+    for (const row of seatsArray) {
+        const { row: rowName, seats } = row;
+        const arrangedRow = Array.from({ length: maxSeatNumber }).fill(-1);
+        // Fill in the seats for the current row
+        for (let i = 0; i < seats.length; i++) {
+            arrangedRow[seats[i] - 1] = i + 1;
+        }
+        // Update the seats for the current row in the arrangedSeats array
+        arrangedSeats.push({ row: rowName, seats: arrangedRow });
+    }
+    return arrangedSeats;
+};
+
+
 const registerCinema = async (req, res) => {
     try {
         req.body.owner = req.Id;
+        req.body.screen = req.body.screen.map((item) => arrangeSeats(item.seatmap))
+        console.log("arranged map: ", JSON.stringify(req.body.screen));
         const cinema = new Cinema(req.body);
         const result = await cinema.save();
         if (result) {
