e06c4b3421f24f411f35edc577e15c7c81534bf1
Ratan Deep Singh <91245947+ratan2020cs131@users.noreply.github.com>
2024-03-29 17:04:41 +0530

    location api bug fixed

--- backend/src/controller/LocationController.js
+++ backend/src/controller/LocationController.js
@@ -10,7 +10,7 @@ const getAddress = async (req, res) => {
         const addressRes = await axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${process.env.OPENCAGE_KEY}`)
         if (addressRes) {
             res.send({
-                city: addressRes.data.results[0].components.city,
+                city: addressRes.data.results[0].components.city ? addressRes.data.results[0].components.city : addressRes.data.results[0].components._normalized_city,
                 zipcode: addressRes.data.results[0].components.postcode,
                 state: addressRes.data.results[0].components.state,
                 country: addressRes.data.results[0].components.country
